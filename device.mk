bb7c18512beba0afe691e623535d435fbddfba15
Sonal Singh <sonal.singh.19993@gmail.com>
2019-03-05 11:23:12 +0000

    X01BD: Move Power HIDL Hal to passthrough

--- device.mk
+++ device.mk
@@ -401,9 +401,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
 # Power
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
-	
+#PRODUCT_PACKAGES += \
+#    android.hardware.power@1.1-service-qti
+
 # RenderScript HAL
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
